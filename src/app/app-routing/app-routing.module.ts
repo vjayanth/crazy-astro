362b5c2bfba6994a25d32bf9b5b1953655596c21
jayanth <vjayanth1291@gmail.com>
2017-05-23 23:18:52 +0530

    added login , register and dashboard components

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import {RouterModule,Routes} from '@angular/router';
+
+import {DashboardComponent} from '../dashboard/dashboard.component';
+import {LoginComponent} from '../login/login.component';
+import {RegisterComponent} from '../register/register.component';
+
+const appRoutes:Routes=[
+  {
+    path:'app',
+    children:[
+      {
+        path:'dashboard',
+        component:DashboardComponent
+          
+      }
+    ]
+  },{
+    path:'login',
+    component:LoginComponent
+  },{
+    path:'register',
+    component:RegisterComponent
+  },{
+    path:'',redirectTo:'/dashboard',pathMatch:'full'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports:[RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {AppRoutingModule} from './app-routing/app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
